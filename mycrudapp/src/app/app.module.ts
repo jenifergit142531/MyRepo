c0ae9a2d936b3b8f2f8caad42cb9fa03b8ac126d
jenifery <jenifers@revature.com>
2023-01-10 15:15:23 -0500

    first

---
+++ mycrudapp/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FruitsModule } from './fruits/fruits.module';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FruitsModule,
+        HttpClientModule
+      
+        
+    ]
+})
+export class AppModule { }
