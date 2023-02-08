2603d8f3eef94c05ccb37af17bb2cef678a9e713
jenifery <jenifers@revature.com>
2023-02-07 20:12:25 -0500

    first

---
+++ WebApplication7/Context/DbContextClass.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication7.Models;
+
+namespace WebApplication7.Context
+{
+    public class DbContextClass : DbContext
+    {
+        public DbContextClass(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WebApplication7/Services/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using WebApplication7.Models;
+
+namespace WebApplication7.Services
+{
+    public interface IProductService
+    {
+        public IEnumerable<Product> GetProductList();
+        public Product GetProductById(int id);
+        public Product AddProduct(Product product);
+        public Product UpdateProduct(Product product);
+        public bool DeleteProduct(int Id);
+    }
+}
